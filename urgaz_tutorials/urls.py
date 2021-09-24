fdb6f1098facd86901c66a92f0e8f0a89ae9e954
xmas1256 <xmas59233@gmail.com>
2021-09-24 21:40:17 +0500

    add statistic url

--- urgaz_tutorials/urls.py
+++ urgaz_tutorials/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from os import name
 from django.contrib import admin
 from django.urls import path
